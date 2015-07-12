aa8721c004bbb0772885f90bb51f66b2f241ae30
Chris Beaman <chris.beaman@gmail.com>
2015-07-12 16:51:03 -0500

    Updated the content and CSS across all pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$(".fancybox").fancybox({
+		width	: 600,
 		helpers : {
 			title : {
 				type : 'over'
@@ -11,54 +12,18 @@ $(document).ready(function() {
 			}
 		}
 	});
-	$(".feedback").fancybox({
-		maxWidth	: 610,
-		maxHeight	: 600,
-		helpers : {
-			overlay : {
-				css : {
-					'background' : 'rgba(0, 0, 0, 0.7)'
-				}
-			}
-		}
-	});
 	$(function () {
-		$("#nav").tinyNav();
+		$('.parentcats').tinyNav({
+			active: 'selected'
+		});
 	});
 	$('html').addClass('js');
 	$(function() {
-		$( "#nav" ).accordion({
+		$( ".parentcats" ).accordion({
 			collapsible: true
 		});
 	});
-	// When any a in the navigation is clicked, evaluate this function
-	$('.sidebar #nav a').click(function(){
-		// The text of the link
-		var text = $( this ).text();
-		// If another is selected, get an array of all of the profiles/entities
-		var $entityArray = $('.entity').toArray();
-		if (text.toUpperCase().indexOf("ALL") != -1)
-		{
-			// If "All" is selected, show all
-			$($entityArray).removeClass('hide',700);
-		}
-		else
-		{
-			// For each of the categories that were listed for each of the profiles
-			$('.entity .categories li a').each(function(){
-				// If the text of the link is the same as the navigation link selected
-				if (text.toUpperCase() == $(this).text().toUpperCase())
-				{
-					// Find the closest ancestor with the .entity class
-					var myProfile = $(this).closest('.entity');
-					// Remove it from the array
-					$entityArray = $.grep($entityArray, function(value) {
-					  return  value.innerHTML != myProfile[0].innerHTML;
-					});
-				}
-			});
-			// Finally, hide every element in the array of profiles without the search string
-			$($entityArray).addClass('hide',700);
-		}
+	$( ".contactme" ).click(function() {
+		$(this).next( ".links" ).slideToggle( "300" );
 	});
 });
